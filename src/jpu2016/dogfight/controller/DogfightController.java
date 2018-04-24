91de3434761809e19b205781c8e54e614371292e
Mmouky <33157839+Mmouky@users.noreply.github.com>
2018-04-24 10:02:36 +0200

    Erreur 3 Phase 2

--- src/jpu2016/dogfight/controller/DogfightController.java
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -8,7 +8,6 @@ public class DogfightController implements IOrderPerformer {
 
 	public DogfightController(IDogfightModel dogfightModel) {
 		this.dogfightModel = dogfightModel;
-		
 	}
 
 	@Override
