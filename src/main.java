c23ebf4078162fed10783b03aa3a8f2dddb98d5b
Mmouky <33157839+Mmouky@users.noreply.github.com>
2018-04-24 13:36:33 +0200

    Phase 2 (Normalement OK)

--- src/main.java
+++ src/main.java
@@ -1,8 +1,16 @@
+import jpu2016.dogfight.controller.DogfightController;
+import jpu2016.dogfight.model.DogFightModel;
+import jpu2016.dogfight.view.DogfightView;
 
 public class main {
 
 	public static void main(String[] args) {
-		
+		final DogFightModel dogfightModel = new DogFightModel();
+		final DogfightController dogfightController = new DogfightController(dogfightModel);
+		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel,
+		dogfightModel);
+		dogfightController.setViewSystem(dogfightView);
+		dogfightController.play();
 	}
 
 }
