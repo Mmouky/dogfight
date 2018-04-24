bb7783aab7c8aa986c982745938f3310df4812f1
CptBaroud <gurvan.seveno@gmail.com>
2018-04-24 10:04:46 +0200

    Update de GameFrame

--- src/jpu2016/dogfight/gameframe/GameFrame.java
+++ src/jpu2016/dogfight/gameframe/GameFrame.java
@@ -1,4 +1,24 @@
 package jpu2016.dogfight.gameframe;
 
+import java.awt.event.KeyEvent;
+import java.util.Observable;
+
 public class GameFrame {
+
+    public GameFrame(String title, IEventPerformer performer, IGraphicsBuilder graphicsBuilder, Observable observable){
+
+    }
+
+    public void keyPressed(KeyEvent keyEvent){
+
+    }
+
+    public void keyReleased(KeyEvent keyEvent){
+
+    }
+
+    public void keyTyped(KeyEvent keyEvent){
+
+    }
+
 }
